787f35e8a3ac9b2527e06b8e6ededbc4ba07763e
Jonathan Dursi <jonathan@dursi.ca>
2017-05-22 14:08:24 -0400

    Python version

---
+++ examples/kmercount/kmercount.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import argparse
+import collections
+
+def kmers_from_sequence(sequence, k):
+    kmers = []
+    for i in range(len(sequence)-k+1):
+        kmers.append(sequence[i:i+k])
+    return kmers
+
+
+def kmer_counts(filename, k):
+    sequences = readfasta(filename)
+    counts = collections.defaultdict(int)
+    for sequence in sequences:
+        for kmer in kmers_from_sequence(sequence, k):
+            counts[kmer] += 1
+    return counts
+
+
+def readfasta(filename):
+    sequences = []
+    cursequence = ""
+
+    def updatelists():
+        if len(cursequence) is not 0:
+            sequences.append(cursequence)
+
+    with open(filename, 'r') as infile:
+        for line in infile:
+            if line[0] == ">":
+                updatelists()
+                cursequence = ""
+            else:
+                cursequence += line.strip()
+
+        updatelists()
+
+    return sequences
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', default="input.fa")
+    parser.add_argument('-k', type=int, default=11)
+    args = parser.parse_args()
+
+    counts = kmer_counts(args.input, args.k)
+    for kmer, count in counts.items():
+        print(kmer, count)
