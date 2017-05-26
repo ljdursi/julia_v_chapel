31efb1d1066f347e2aa87c34ed60464c3c2ed34e
Jonathan Dursi <jonathan@dursi.ca>
2017-05-25 22:17:30 -0400

    tabs

--- examples/kmercount/kmercount.jl
+++ examples/kmercount/kmercount.jl
@@ -1,8 +1,8 @@
 function kmer_count(infile::String, k::Int)
-	sequences = read_sequences(infile)
+    sequences = read_sequences(infile)
 
-	counts = Dict{String, Int8}()
-	for seq in sequences
+    counts = Dict{String, Int8}()
+    for seq in sequences
         for i = 1:length(seq)-k+1
             kmer = seq[i : i+k-1]
             if haskey(counts, kmer)
@@ -11,44 +11,44 @@ function kmer_count(infile::String, k::Int)
                 counts[kmer] = 1
             end
         end
-	end 
+    end 
 
-	for kmer in eachindex(counts)
-		println(kmer, ' ', counts[kmer])
-	end
+    for kmer in eachindex(counts)
+        println(kmer, ' ', counts[kmer])
+    end
 end
 
 function read_sequences(input_filename::String)
-	sequences = String[]
-	inseq = false
-	currentseq = [""]
+    sequences = String[]
+    inseq = false
+    currentseq = [""]
     input = open(input_filename, "r")
     for line in eachline(input)
         if line[1] == '>'
-			if inseq
-				push!(sequences, join(currentseq))
-				currentseq = [""]
-			end
-			inseq = false
+            if inseq
+                push!(sequences, join(currentseq))
+                currentseq = [""]
+            end
+            inseq = false
         else
-        	inseq = true
-        	push!(currentseq, line)
+            inseq = true
+            push!(currentseq, line)
         end
     end
     if inseq
-		push!(sequences, join(currentseq))
-	end
-	return sequences
+        push!(sequences, join(currentseq))
+    end
+    return sequences
 end
 
 input_filename = "input.fa"
 k = 11
 nargs = length(ARGS)
 if nargs > 0
-	input_filename = ARGS[1]
+    input_filename = ARGS[1]
 end
 if nargs > 1
-	k = parse(Int8, ARGS[2])
+    k = parse(Int8, ARGS[2])
 end
 
 kmer_count(input_filename, k)
