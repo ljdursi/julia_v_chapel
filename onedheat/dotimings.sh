cac65acb10683771417a9537ef48e69eee06626d
Jonathan Dursi <jonathan@dursi.ca>
2017-05-30 19:10:49 +0000

    Updated timings (c4.8x)

--- onedheat/dotimings.sh
+++ onedheat/dotimings.sh
@@ -9,9 +9,11 @@ echo ""
 echo "# Chapel"
 echo "## Compile:"
 export CHPL_TARGET_ARCH="native"
+unset CHPL_COMM
+unset GASNET_SPAWNFN
 time chpl --fast ${BASE}.chpl -o ${BASE}_chpl 
 echo "## Run:"
-time ./${BASE}_chpl 
+time ./${BASE}_chpl --dataParTasksPerLocale=1
 
 echo ""
 echo "# Python"
